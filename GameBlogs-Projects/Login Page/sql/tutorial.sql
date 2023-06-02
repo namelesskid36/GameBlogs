f5d408e5418e95833760bb8864dc5f81e32bcadc
ThapaDhiraj <134988912+ThapaDhiraj@users.noreply.github.com>
2023-06-02 08:16:21 +0545

    Add files via upload

---
+++ GameBlogs-Projects/Login Page/sql/tutorial.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users(
+    Id int PRIMARY KEY AUTO_INCREMENT,
+    Username varchar(200),
+    Email varchar(200),
+    Age int,
+    Password varchar(200)
+);
+
+
+database name loginpage
+ani 
+table ko chai users
+ 
