04012e34259bc9bbe67175b5486244ea0477912b
Sudip Awasthi <133966593+namelesskid36@users.noreply.github.com>
2023-06-14 07:34:35 +0545

    Add files via upload

---
+++ GameBlogs-Projects/footer/index.php
@@ -0,0 +1,27 @@
+<div class="allf">
+  <div class="bodyf">
+    <footer class="footerf">5
+      <div class="rowf">
+        <div class="columnf">
+          <h4 class="h4f">About Us</h4>
+          <p class="pf">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in metus euismod, faucibus metus
+            ut, semper nibh.
+            Aenean euismod justo eu enim dapibus suscipit.</p>
+        </div>
+        <div class="columnf">
+          <h4 class="h4f">Quick Links</h4>
+          <ul class="ulf">
+            <li class="lif"><a class="af" href="home.php"><i class="if" class="fa fa-angle-right"></i> Home</a></li>
+            <li class="lif"><a class="af" href="#"><i class="if" class="fa fa-angle-right"></i> Contact us</a></li>
+            <li class="lif"><a class="af" href="edit.php"><i class="if" class="fa fa-angle-right"></i> Profile</a></li>
+          </ul>
+        </div>
+        <div class="columnf">
+          <h4 class="h4f">Connect with Us:</h4>
+          <?php include("./social/social.html"); ?>
+        </div>
+      </div>
+      <p class="copyrightf">© 2023 All Rights Reserved</p>
+    </footer>
+  </div>
+</div>
