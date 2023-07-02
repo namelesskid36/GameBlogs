427a50533a8e029014e6aae2d7239c30bca0320e
Sudip Awasthi <133966593+namelesskid36@users.noreply.github.com>
2023-07-02 22:28:17 +0545

    Delete GameBlogs-Projects directory

--- GameBlogs-Projects/about/index.php
+++
@@ -1,9 +0,0 @@
-<section class="heroa" style="background-image: url(https://cdn.wallpapersafari.com/22/72/0TDxAP.jpg);">
-  <div class="diva">
-    <h1 class="h1a" style="color: white;" >About US</h1>
-    <p class="pa" style="color: white;" >Welcome to Game Hub! We are a passionate team of gamers dedicated to providing you with the best gaming content and experiences. Our mission is to create a vibrant community where gamers of all levels can come together to share their love for gaming. Whether you're a seasoned player or just starting out, we've got something for everyone. From game reviews and tutorials to gaming news and discussions, we strive to bring you valuable and engaging content. Join us on this exciting journey as we explore the world of gaming and unleash the power of play!</p>
-    <a class="cta" href="./about/slider about us/index.html">Contact us</a>
-    
-  </div>
-  <img class="imga" src="https://img.favpng.com/0/22/22/computer-icons-users-group-png-favpng-ZDTNBLEd2hfdJpnS2DippVpw0.jpg" alt="">
-</section>
